e5efe09474605af0ca4956e3843ce2a9b8cf6bc1
Matt Frazier <maf7sm@virginia.edu>
2016-08-04 01:44:12 -0400

    Remove unnecessary error     -raised if require fails

--- flunch_bot.js
+++ flunch_bot.js
@@ -12,10 +12,6 @@ var localSettings = require('./local.json');
 var flunchBot = require('./flunchBotModel').flunchBot;
 var factualClient = require('./factualApiClient').factualClient;
 
-if (!localSettings) {
-    console.log('Error: Specify local settings in `local.json`');
-}
-
 for (var key in localSettings) {
     if (localSettings[key] !== undefined){  // ignore key:null mapping
         settings[key] = localSettings[key];
