acc85348219e2cc3b7a46facd6a39590a6b87475
Matt Frazier <maf7sm@virginia.edu>
2016-08-08 22:25:49 -0400

    Use moment.js to show hours     -fix off-by-one

--- factualApiClient.js
+++ factualApiClient.js
@@ -1,17 +1,18 @@
 var Factual = require('factual-api');
+var moment = require('moment');
 
 function getRandomIntList(min, max, count) {
     min = Math.ceil(min);
     max = Math.floor(max);
     list = [];
-    if (max === count) {
-        for (var i = min; i <= max; i++) {
+    if (max === count - 1) {
+        for (var i = 0; i < max; i++) {
             list.push(i);
         }
         return list;
     }
     while (list.length < count) {
-        int = Math.floor(Math.random() * (max - min + 1)) + min;
+        int = Math.floor(Math.random() * (max - min + 1));
         if (list.indexOf(int) === -1) {
             list.push(int);
         }
@@ -40,7 +41,10 @@ factualClient.prototype.findRestaurants = function(count, cb){
     // TODO: improve this
     return this.api.get('/t/places-us',{
             geo:{"$within":{"$rect":[[38.032647,-78.484397],[38.027087,-78.476576]]}},
-            filters:{category_ids:{"$includes_any":[312,347]}},
+            filters:{category_ids:{"$includes_any":[
+                347, // Restaurants
+                // 312, // Bars
+            ]}},
             limit:50
         }, function (error, res) {
 
@@ -50,7 +54,23 @@ factualClient.prototype.findRestaurants = function(count, cb){
         var restaurants = '';
         for (var i = 0; i < indices.length; i++) {
             restaurant = res.data[indices[i]];
-            restaurants += '* ' + restaurant.name + ' (' + restaurant.address + ')\n';
+            var hours;
+            try {
+                hours = restaurant.hours[moment().format('dddd').toLowerCase()];
+                var h  =  '';
+                for (var j=0; j < hours.length; j++) {
+                    h = h + hours[j][0] + '-' + hours[j][1] + ','; 
+                }
+                hours = h;
+            } catch (err) {
+                // Maybe there are no specified hours for today
+                // List general hours, if any
+                hours = restaurant.hours_display;
+            }
+            if (!hours) {
+                hours = 'Hours unknown';
+            }
+            restaurants += '* ' + restaurant.name + ': ' + hours + '\n';
         }
         console.log(restaurants);
         return cb(restaurants);
